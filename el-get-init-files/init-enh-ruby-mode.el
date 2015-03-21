930230fd7b6a76e202c131a62f8364731e346d2d
Mohit Thatte <mohit.thatte@gmail.com>
2015-03-21 19:23:04 +0530

    Dont use enh-ruby faces

--- el-get-init-files/init-enh-ruby-mode.el
+++ el-get-init-files/init-enh-ruby-mode.el
@@ -2,4 +2,5 @@
 (add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
 (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
 
+(remove-hook 'enh-ruby-mode-hook 'erm-define-faces)
 (add-hook 'enh-ruby-mode-hook 'rinari-minor-mode)
