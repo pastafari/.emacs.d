06707db140f28778ab59386813151072ca33c854
Mohit Thatte <mohit.thatte@gmail.com>
2015-05-02 14:33:55 +0530

    Add html.erb to ffip-patterns

--- el-get-init-files/init-find-file-in-project.el
+++ el-get-init-files/init-find-file-in-project.el
@@ -1,1 +1,7 @@
 (global-set-key (kbd "C-x f") 'find-file-in-project)
+
+(eval-after-load 'find-file-in-project
+  '(progn
+     (add-to-list 'ffip-patterns "*.html.erb" t)))
+
+(provide 'init-find-file-in-project)
