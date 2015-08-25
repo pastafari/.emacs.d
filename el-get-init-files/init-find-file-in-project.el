10fcba9e5969c893b97f02a276c36130fbe9122e
Mohit Thatte <pastafari@Mohits-MacBook-Pro.local>
2015-08-25 16:16:18 +0530

    Add more file types to ffip

--- el-get-init-files/init-find-file-in-project.el
+++ el-get-init-files/init-find-file-in-project.el
@@ -3,6 +3,9 @@
 (eval-after-load 'find-file-in-project
   '(progn
      (add-to-list 'ffip-patterns "*.html.erb" t)
-     (add-to-list 'ffip-patterns "*.scss" t)))
+     (add-to-list 'ffip-patterns "*.scss" t)
+		 (add-to-list 'ffip-patterns "*.js" t)
+		 (add-to-list 'ffip-patterns "*.rb" t)
+		 (add-to-list 'ffip-patterns "*.clj" t)))
 
 (provide 'init-find-file-in-project)
