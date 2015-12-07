39ce85898d2e28bdd54e47e4aa4151dfcd99fbdc
Mohit Thatte <mohit.thatte@gmail.com>
2015-12-07 14:53:14 +0530

    Set font size to 16

--- el-get-init-files/init-darcula-theme.el
+++ el-get-init-files/init-darcula-theme.el
@@ -2,4 +2,8 @@
 ;; Because Monaco is awesome!
 (set-frame-font "Monaco-16")
 
+;; Set font-size to 16.
+;; http://stackoverflow.com/questions/294664/how-to-set-the-font-size-in-emacs
+(set-face-attribute 'default nil :height 160)
+
 (provide 'init-darcula-theme)
