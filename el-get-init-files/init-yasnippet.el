757c9f2e519cdbf82477c252343c0363d887a7cd
Mohit Thatte <mohit.thatte@gmail.com>
2016-02-28 17:14:34 +0530

    Do not use TAB for yas-expand

--- el-get-init-files/init-yasnippet.el
+++ el-get-init-files/init-yasnippet.el
@@ -3,5 +3,8 @@
 											 "~/.emacs.d/snippets")))
 
 (yas-global-mode 1)
+(define-key yas-minor-mode-map (kbd "<tab>") nil)
+(define-key yas-minor-mode-map (kbd "TAB") nil)
+(define-key yas-minor-mode-map (kbd "M-/") 'yas-expand)
 
 (provide 'init-yasnippet)
