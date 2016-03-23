b8bf6166dfb140d9b827a004d12b4a06622b3279
Mohit Thatte <mohit.thatte@gmail.com>
2016-03-23 11:21:08 +0530

    Indent by 2 in web mode

--- el-get-init-files/init-web-mode.el
+++ el-get-init-files/init-web-mode.el
@@ -2,13 +2,15 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
 
-(setq-default web-mode-enable-auto-pairing t)
-(setq-default web-mode-enable-css-colorization t)
-(setq-default web-mode-enable-current-column-highlight t)
-(setq-default web-mode-enable-current-element-highlight t)
+(defun web-mode-customizations ()
+  (setq-default web-mode-enable-auto-pairing t)
+  (setq-default web-mode-enable-css-colorization t)
+  (setq-default web-mode-enable-current-column-highlight t)
+  (setq-default web-mode-enable-current-element-highlight t)
+  (setq-default web-mode-markup-indent-offset 2)
+  (setq-default web-mode-css-indent-offset 2)
+  (setq-default web-mode-code-indent-offset 2))
 
-(setq-default web-mode-markup-indent-offset 2)
-(setq-default web-mode-css-indent-offset 2)
-(setq-default web-mode-code-indent-offset 2)
+(add-hook 'web-mode-hook 'web-mode-customizations)
 
 (provide 'init-web-mode)
